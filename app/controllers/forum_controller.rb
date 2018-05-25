56aa84ea88c00e3945529286e44253d9e5c9b62f
Angel Plascencia <aplascencia4@cps.edu>
2018-05-25 19:35:57 +0000

    HTML & CSS working + start of variable implementation

--- app/controllers/forum_controller.rb
+++ app/controllers/forum_controller.rb
@@ -3,4 +3,10 @@ class ForumController < ApplicationController
   def index
   end
   
+  def show
+     @forum_description = ""
+    # Examples on how to call variables:
+    # <%= @forum_description %> <%= @todo_pomodoro_estimate %>
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get 'forum/index', to: 'forum#index'
+  get 'forum/topic', to: 'forum#topic'
 end
